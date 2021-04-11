5195d7d4effb091c6175910fb03ae3db5a508fc6
Ralph Langendam <rlangendam@gmail.com>
2021-04-11 02:11:41 +0200

    Add survival factor and clamp mutated weights and biases

--- EvolutionaryAlgorithm.py
+++ EvolutionaryAlgorithm.py
@@ -10,13 +10,14 @@ class EvolutionaryAlgorithm:
     def initialize_population(self, agent, size):
         return list(map(agent, range(size)))
 
-    def run(self, agent, gym, size=10, generation_count=10, selection_factor=0.5, agent_timeout=1000):
+    def run(self, agent, gym, size=10, generation_count=10, selection_factor=0.5, agent_timeout=2000,
+            survival_factor=0.5):
         population = self.initialize_population(agent, size)
         for generation in range(generation_count):
             fitnesses = list(map(self.compute_fitness(gym, agent_timeout), population))
             self.set_average_fitness(fitnesses)
             population = self.select_fittest(zip(population, fitnesses), selection_factor * size)
-            population = self.recombine(population, size)
+            population = self.recombine(population, size, survival_factor)
             population = self.mutate(population)
         print(self.average_fitness)
 
@@ -39,8 +40,8 @@ class EvolutionaryAlgorithm:
         self.average_fitness.append(sum(fitnesses) / len(fitnesses))
         print(self.average_fitness)
 
-    def recombine(self, population, size):
-        new_population = population
+    def recombine(self, population, size, survival_factor):
+        new_population = population[:int(len(population) * survival_factor)]
 
         if len(population) % 2 == 1:
             population = population[1:]
@@ -73,15 +74,22 @@ class EvolutionaryAlgorithm:
         return new_population[:size]
 
     def mutate(self, population):
-        # agent.model.layers[0].get_weights() : [0] weights, [1] biases
+        def add_in_range(val, add, minval, maxval):
+            newval = val + add
+            if newval < minval:
+                return minval
+            if newval > maxval:
+                return maxval
+            return newval
+
         def mutate_layer(layer, mutation_frequency=0.01, mutation_strength=0.01):
             weights, biases = layer.get_weights()
             for i in map(lambda _: random.randrange(0, len(weights)), range(int(len(weights) * mutation_frequency))):
                 for j in map(lambda _: random.randrange(0, len(weights[0])),
                              range(int(len(weights[0]) * mutation_frequency))):
-                    weights[i][j] += random.rand() * mutation_strength
+                    weights[i][j] = add_in_range(weights[i][j], random.uniform(-1.0, 1.0) * mutation_strength, 0, 1)
             for i in map(lambda _: random.randrange(0, len(biases)), range(int(len(biases) * mutation_frequency))):
-                biases[i] += random.rand() * mutation_strength
+                biases[i] = add_in_range(biases[i], random.uniform(-1.0, 1.0) * mutation_strength, 0, 1)
             return {"weights": weights, "biases": biases}
 
         def mutate_agent(agent):
