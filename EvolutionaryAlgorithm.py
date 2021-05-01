04f5f120bec7c3d2c32aef07ab9d77bc668b3c7e
Ralph Langendam <rlangendam@gmail.com>
2021-05-01 22:35:03 +0200

    Make testable

--- EvolutionaryAlgorithm.py
+++ EvolutionaryAlgorithm.py
@@ -2,6 +2,7 @@ import random
 from SequentialAgent import SequentialAgent
 import numpy as np
 from time import time
+from functools import partial
 
 from StateTransform import MarioNotFoundException
 
@@ -16,14 +17,14 @@ class EvolutionaryAlgorithm:
         return list(map(agent, range(size)))
 
     def run(self, agent, gym, size=100, generation_count=10, selection_factor=0.04, agent_timeout=10000,
-            survival_factor=0.5):
+            survival_factor=0.5, sigma=0.2):
         population = self.initialize_population(agent, size)
         for generation in range(generation_count):
             fitnesses = list(map(self.compute_fitness(gym, agent_timeout), population))
             print(np.max(fitnesses))
             self.set_average_fitness(fitnesses)
             population = self.select_fittest(zip(population, fitnesses), selection_factor * size)
-            population = self.recombine(population, size, survival_factor)
+            population = self.recombine(population, size, survival_factor, SequentialAgent, sigma)
             # population = self.mutate(population)
         print(self.average_fitness)
 
@@ -62,36 +63,47 @@ class EvolutionaryAlgorithm:
         print(self.average_fitness)
 
     @staticmethod
-    def recombine(population, size, survival_factor):
+    def recombine_tensor(sigma, tensors):
+        if isinstance(tensors[0], np.ndarray):
+            return list(map(partial(EvolutionaryAlgorithm.recombine_tensor, sigma), zip(tensors[0], tensors[1])))
+        else:
+            mu = (tensors[0] + tensors[1]) / 2
+            # print(mu)
+            result = random.gauss(mu=mu, sigma=max(abs(tensors[0] - mu) / 2, sigma))
+            return result
+
+    @staticmethod
+    def recombine_layers(sigma, layers):
+        left_weights, left_biases = layers[0].get_weights()
+        right_weights, right_biases = layers[1].get_weights()
+
+        return {"weights": (
+            list(map(partial(EvolutionaryAlgorithm.recombine_tensor, sigma), zip(left_weights, right_weights)))),
+            "biases": (
+                list(map(partial(EvolutionaryAlgorithm.recombine_tensor, sigma), zip(left_biases, right_biases))))}
+
+    @staticmethod
+    def recombine_parents(agent, sigma, parents):
+        return agent(
+            list(map(partial(EvolutionaryAlgorithm.recombine_layers, sigma),
+                     zip(parents[0].model.layers, parents[1].model.layers))))
+
+    @staticmethod
+    def shuffle_list(list_to_shuffle):
+        random.shuffle(list_to_shuffle, random.random)
+        return list_to_shuffle
+
+    @staticmethod
+    def recombine(population, size, survival_factor, agent, sigma):
         new_population = population[:int(len(population) * survival_factor)]
 
         if len(population) % 2 == 1:
-            population = population[:-1]
-
-        def recombine_tensor(tensors):
-            if isinstance(tensors[0], np.ndarray):
-                return list(map(recombine_tensor, zip(tensors[0], tensors[1])))
-            else:
-                mu = (tensors[0] + tensors[1]) / 2
-                # print(mu)
-                return random.gauss(mu=mu, sigma=max(abs(tensors[0] - mu), 0.2))
-
-        def recombine_layers(layers):
-            left_weights, left_biases = layers[0].get_weights()
-            right_weights, right_biases = layers[1].get_weights()
-
-            return {"weights": (list(map(recombine_tensor, zip(left_weights, right_weights)))),
-                    "biases": (list(map(recombine_tensor, zip(left_biases, right_biases))))}
-
-        def recombine_parents(parents):
-            return SequentialAgent(list(map(recombine_layers, zip(parents[0].model.layers, parents[1].model.layers))))
-
-        def shuffle_list(list_to_shuffle):
-            random.shuffle(list_to_shuffle, random.random)
-            return list_to_shuffle
+            population = population[1:]
 
         while len(new_population) < size:
-            new_population.extend(map(recombine_parents, zip(shuffle_list(population), shuffle_list(population))))
+            new_population.extend(map(partial(EvolutionaryAlgorithm.recombine_parents, agent, sigma),
+                                      zip(EvolutionaryAlgorithm.shuffle_list(population),
+                                          EvolutionaryAlgorithm.shuffle_list(population))))
 
         return new_population[:size]
 
