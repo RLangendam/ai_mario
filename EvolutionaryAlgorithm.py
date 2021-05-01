889097c19f42c6867e8c0bb2799441f3e4c50a90
Kasimir van Rijn <kajaisback@hotmail.com>
2021-05-01 23:01:46 +0200

    jemoeder2

--- EvolutionaryAlgorithm.py
+++ EvolutionaryAlgorithm.py
@@ -16,16 +16,15 @@ class EvolutionaryAlgorithm:
     def initialize_population(agent, size):
         return list(map(agent, range(size)))
 
-    def run(self, agent, gym, size=100, generation_count=10, selection_factor=0.04, agent_timeout=10000,
+    def run(self, agent, gyms, size=100, generation_count=10, selection_factor=0.04, agent_timeout=10000,
             survival_factor=0.5, sigma=0.2):
         population = self.initialize_population(agent, size)
         for generation in range(generation_count):
-            fitnesses = list(map(self.compute_fitness(gym, agent_timeout), population))
+            fitnesses = list(map(self.compute_fitness(gyms[0], agent_timeout), population))  # adjust
             print(np.max(fitnesses))
             self.set_average_fitness(fitnesses)
             population = self.select_fittest(zip(population, fitnesses), selection_factor * size)
             population = self.recombine(population, size, survival_factor, SequentialAgent, sigma)
-            # population = self.mutate(population)
         print(self.average_fitness)
 
     @staticmethod
@@ -106,28 +105,3 @@ class EvolutionaryAlgorithm:
                                           EvolutionaryAlgorithm.shuffle_list(population))))
 
         return new_population[:size]
-
-    @staticmethod
-    def mutate(population):
-        def add_in_range(val, add, minval, maxval):
-            newval = val + add
-            if newval < minval:
-                return minval
-            if newval > maxval:
-                return maxval
-            return newval
-
-        def mutate_layer(layer, mutation_frequency=0.01, mutation_strength=0.01):
-            weights, biases = layer.get_weights()
-            for i in map(lambda _: random.randrange(0, len(weights)), range(int(len(weights) * mutation_frequency))):
-                for j in map(lambda _: random.randrange(0, len(weights[0])),
-                             range(int(len(weights[0]) * mutation_frequency))):
-                    weights[i][j] = add_in_range(weights[i][j], random.uniform(-1.0, 1.0) * mutation_strength, 0, 1)
-            for i in map(lambda _: random.randrange(0, len(biases)), range(int(len(biases) * mutation_frequency))):
-                biases[i] = add_in_range(biases[i], random.uniform(-1.0, 1.0) * mutation_strength, 0, 1)
-            return {"weights": weights, "biases": biases}
-
-        def mutate_agent(agent):
-            return SequentialAgent(list(map(mutate_layer, agent.model.layers)))
-
-        return list(map(mutate_agent, population))
--- mario.py
+++ mario.py
@@ -6,23 +6,24 @@ from EvolutionaryAlgorithm import EvolutionaryAlgorithm
 
 
 class AiMario:
-    def __init__(self):
+    def __init__(self, number_of_gyms=2):
         quiet = "--quiet" in sys.argv
-        self.pyboy = PyBoy(rom, window_type="headless" if quiet else "SDL2", window_scale=3, debug=not quiet,
-                           game_wrapper=True)
+        self.pyboys = [PyBoy(rom, window_type="headless" if quiet else "SDL2", window_scale=3, debug=not quiet,
+                             game_wrapper=True) for _ in range(number_of_gyms)]
 
     def run(self):
-        self.pyboy.set_emulation_speed(0)
-        assert self.pyboy.cartridge_title() == "SUPER MARIOLAN"
+        for pyboy in self.pyboys:
+            pyboy.set_emulation_speed(0)
+            assert pyboy.cartridge_title() == "SUPER MARIOLAN"
+            mario = pyboy.game_wrapper()
 
-        mario = self.pyboy.game_wrapper()
-        gym = self.pyboy.openai_gym(
-            observation_type="minimal", action_type="all", simultaneous_actions=False)
+        gyms = [pyboy.openai_gym(observation_type="minimal", action_type="all", simultaneous_actions=False)
+                for pyboy in self.pyboys]
 
         mario_algorithm = EvolutionaryAlgorithm()
-        mario_algorithm.run(agent=lambda _: SequentialAgent(), gym=gym)
+        mario_algorithm.run(agent=lambda _: SequentialAgent(), gyms=gyms)
 
-        self.pyboy.stop()
+        self.pyboys.stop()
 
 
 def main():
