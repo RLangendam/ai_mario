731bf5da8d172fc144c3bc8eb01c488b3e348eb4
Ralph Langendam <rlangendam@gmail.com>
2021-05-02 02:36:54 +0200

    Upgrade SDL version

--- bootstrap.py
+++ bootstrap.py
@@ -9,10 +9,11 @@ def download(url, filename):
         urllib.request.urlretrieve(url + filename, filename)
 
 
-sdl_archive = "SDL2-devel-2.0.10-VC.zip"
+sdl_version = "2.0.14"
+sdl_archive = f'SDL2-devel-{sdl_version}-VC.zip'
 download("https://www.libsdl.org/release/", sdl_archive)
 
-if not os.path.exists("SDL2-2.0.10"):
+if not os.path.exists("SDL2" + sdl_version):
     with zipfile.ZipFile(sdl_archive, 'r') as zip_ref:
         zip_ref.extractall()
 
@@ -23,7 +24,6 @@ download("https://sites.google.com/site/nosurveyemus/the-gameboy-page/", rom)
 file_path = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(0, file_path + "/..")
 
-os.environ["PYSDL2_DLL_PATH"] = os.path.join(file_path, "SDL2-2.0.10/lib/x64")
+os.environ["PYSDL2_DLL_PATH"] = os.path.join(file_path, f'SDL2-{sdl_version}/lib/x64')
 
 from pyboy import PyBoy, WindowEvent
-
